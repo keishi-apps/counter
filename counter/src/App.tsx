b88aa1d4321d773e6a68088ca7e3fff1681a5eb5
keishi-apps <100992094+keishi-apps@users.noreply.github.com>
2025-07-31 20:01:45 +0900

    Add files via upload

---
+++ counter/src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import Counter from "./Counter";
+
+function App() {
+  return (
+    <div className="App">
+      Counterコンポーネント動作確認
+      <Counter initVal={0} />
+    </div>
+  );
+}
+
+export default App;
